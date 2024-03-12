0d9002a7514fcd83909c04f760ad6d7852d6e90e
Naikotekinari <163132808+Naikotekinari@users.noreply.github.com>
2024-03-12 13:08:39 +0000

    test more

--- savedata.js
+++ savedata.js
@@ -2,6 +2,8 @@ export const saveData = [
   [1, 20, 4, 0],
   [0, 14, 0, 0],
   [2, 90, 10, 0],
-  [1, 100, 2, 0]
+  [1, 100, 2, 0],
+  [1, 101, 2, 0],
+  [2, 103, 2, 0]
 ];
 //charater id, level, cons, calculated priority
